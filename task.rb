8036afb5c4864879350aa0d72639725f43d8157c
shinya.muraki <shinya0104@shinyanoMacBook-Pro.local>
2020-07-07 23:53:23 +0900

    q10 update

--- task.rb
+++ task.rb
@@ -89,15 +89,8 @@ end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
-  
-
-  # 以下に回答を記載
   foods.each do |food|
-    if food.index("うに")
-      puts "好物です"
-    else
-      puts "まぁまぁ好きです"
-    end
+      puts food.include?("うに") ? "好物です" : "まぁまぁ好きです"
   end
 
 
