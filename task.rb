a24779d3b0c5013072cc59c96aaa8004803f2b55
shinya.muraki <shinya0104@shinyanoMacBook-Pro.local>
2020-07-07 23:19:26 +0900

    q8 update

--- task.rb
+++ task.rb
@@ -68,9 +68,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map(&:to_s)
-  programming_languages[0..3] = ["Ruby", "Php", "Python", "Javascript"]
-  upper_case_programming_languages = ["RUBY", "PHP", "PYTHON", "JAVASCRIPT"]
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
